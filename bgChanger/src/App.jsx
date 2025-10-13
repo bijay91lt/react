10f18f00e3da84e598c4db81deeca0d58b1275dc
bijay91lt <bijay3990@gmail.com>
2025-10-13 17:59:43 +0545

    button css added

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -10,23 +10,23 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-3xl">
           <button onClick={() => setColor("red")}
-          className="outline-none px-3 py-2 rounded-3xl text-white shadow-lg" style={{backgroundColor:"red"}}>
+          className="min-h-[3rem] px-4  active:scale-105 scale-110 active:transition-all duration-50 font-semibold rounded-full bg-red-100 text-gray-800">
           Red</button>
 
           <button onClick={() => setColor("blue")}
-          className="outline-none px-3 py-2 rounded-3xl text-white shadow-lg" style={{backgroundColor:"blue"}}>
+          className="min-h-[3rem] px-4  active:scale-105 scale-110 active:transition-all duration-50 font-semibold rounded-full bg-blue-100 text-gray-800">
           Blue</button>
 
           <button onClick={() => setColor("green")}
-          className="outline-none px-3 py-2 rounded-3xl text-white shadow-lg" style={{backgroundColor:"green"}}>
+          className="min-h-[3rem] px-4  active:scale-105 scale-110 active:transition-all duration-50 font-semibold rounded-full bg-green-100 text-gray-800">
           Green</button>
 
           <button onClick={() => setColor("yellow")}
-          className="outline-none px-3 py-2 rounded-3xl text-white shadow-lg" style={{backgroundColor:"yellow"}}>
+          className="min-h-[3rem] px-4  active:scale-105 scale-110 active:transition-all duration-50 font-semibold rounded-full bg-yellow-100 text-gray-800">
           Yellow</button>
 
           <button onClick={() => setColor("orange")}
-          className="outline-none px-3 py-2 rounded-3xl text-white shadow-lg" style={{backgroundColor:"orange"}}>
+          className="min-h-[3rem] px-4  active:scale-105 scale-110 active:transition-all duration-50 font-semibold rounded-full bg-orange-100 text-gray-800">
           Orange</button>
         </div>
       </div>
